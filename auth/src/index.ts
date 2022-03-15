08f934bd07264472de638afa45d0484332bcb4ce
sammykirigha <dkirigha18@gmail.com>
2022-03-16 01:36:15 +0300

    modified index.ts file and added load balancer with ingress nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,8 +4,10 @@ import {json} from 'body-parser';
 const app = express();
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi Samuel you made it happen bigups......')
+})
 
 app.listen(3000, () => {
-    console.log('App listening on port 3000!......');
-    
+   console.log('App listening on port 3000!......')
 })

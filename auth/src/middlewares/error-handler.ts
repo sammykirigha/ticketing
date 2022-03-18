0978a1b25479c6588855a0c92449b41904a5623b
sammykirigha <dkirigha18@gmail.com>
2022-03-18 22:34:47 +0300

    added an error-handler file

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,5 +1,6 @@
 import  {Request, Response, NextFunction} from 'express';
 import { CustomeError } from '../errors/custome-error';
+
 export const errorHandler = (
     err:Error, 
     req:Request,

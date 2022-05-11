20705a3e0fd7405e6159e64a8c3394f0bbc9b00b
sammykirigha <dkirigha18@gmail.com>
2022-05-11 19:37:48 +0300

    created a ticket route

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/tickets";
 
 it('it has a router handler listening to /api/tickets for post request', async () => {
 
@@ -47,5 +48,13 @@ it('it return an error if an invalid price is provided', async () => {
 
 
 it('it creates a ticket with valid parameters', async () => {
+	let tickets = await Ticket.find({})
+	expect(tickets.length).toEqual(0)
 
+	const title =  'asdfvlhfv'
+	await request(app).post('/api/tickets').set('Cookie', global.signin()).send({ title, price: 20 }).expect(201)
+	tickets = await Ticket.find({});
+	expect(tickets.length).toEqual(1)
+	expect(tickets[0].price).toEqual(20);
+	expect(tickets[0].title).toEqual(title);
 }) 
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,14 +1,24 @@
 import express, { Request, Response } from 'express';
 import {body} from 'express-validator'
 import { requireAuth, validateRequest } from '@sdktickets/sammy';
+import { Ticket } from '../models/tickets';
 
 const router = express.Router();
 
 router.post('/api/tickets', requireAuth, [
 	body('title').not().isEmpty().withMessage('Title is required'),
 	body('price').isFloat({gt: 0}).withMessage('price is requiredand is greater than zero')
-], validateRequest, (req: Request, res: Response) => {
-	res.sendStatus(200);
+], validateRequest, async (req: Request, res: Response) => {
+	const { title, price } = req.body;
+
+	const ticket = Ticket.build({
+		title,
+		price,
+		userId: req.currentUser!.id
+	});
+	await ticket.save()
+
+	res.status(201).send(ticket)
 })
 
 export {router as createTicketRouter}

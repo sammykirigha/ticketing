602d9241a7f2ce8b6c8d8498e702876a7176a00c
sammykirigha <dkirigha18@gmail.com>
2022-05-11 22:00:22 +0300

    implemented index router to fetch all the tickets created

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { createTicketRouter } from './routes/new'
 import {showTicketRouter} from './routes/show'
+import { indexTicketRouter } from "./routes";
 
 import { errorHandler, NotFoundError, currentUser } from "@sdktickets/sammy";
 
@@ -20,6 +21,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter)
 
 app.all("*", async () => {
     throw new NotFoundError();

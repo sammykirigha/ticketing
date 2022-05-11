7e3950227995dee857a5a1071f13228c6262f41e
sammykirigha <dkirigha18@gmail.com>
2022-05-11 20:46:08 +0300

    implemented the show tickets router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { createTicketRouter} from './routes/new'
+import { createTicketRouter } from './routes/new'
+import {showTicketRouter} from './routes/show'
 
 import { errorHandler, NotFoundError, currentUser } from "@sdktickets/sammy";
 
@@ -18,6 +19,8 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+
 app.all("*", async () => {
     throw new NotFoundError();
 });

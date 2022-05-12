99b05430ddec09ccf82f37832a6c584dadd86cb3
sammykirigha <dkirigha18@gmail.com>
2022-05-12 23:07:53 +0300

    updated the file

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,17 +2,17 @@ import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
 
-it("return s a 404 is the provided doest not exist", async () => {
-    const id = new mongoose.Types.ObjectId().toHexString();
-    await request(app)
-        .put(`/api/tickets/${id}`)
-        .set("Cookie", global.signin())
-        .send({
-            title: "hrbvkh",
-            price: 20,
-        })
-        .expect(404);
-});
+// it("return s a 404 is the provided doest not exist", async () => {
+//     const id = new mongoose.Types.ObjectId().toHexString();
+//     await request(app)
+//         .put(`/api/tickets/${id}`)
+//         .set("Cookie", global.signin())
+//         .send({
+//             title: "hrbvkh",
+//             price: 20,
+//         })
+//         .expect(404);
+// });
 
 // it("return s a 401 if the user is not authenticated", async () => {
 //     const id = new mongoose.Types.ObjectId().toHexString();

2f1eb399766e2a4922648c6d8fc5d8b98d5f0ed1
sammykirigha <dkirigha18@gmail.com>
2022-05-13 23:46:23 +0300

    listening for data

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,17 @@
+import nats from "node-nats-streaming";
+
+console.clear();
+
+const stan = nats.connect('ticketing', '123', { url: 'http://localhost:4222' })
+
+stan.on('connect', () => {
+	console.log('am listening to you guys');
+
+	const subscription = stan.subscribe('ticket:created')
+
+	subscription.on('message', (msg) => {
+		console.log('message received');
+		
+	})
+	
+})

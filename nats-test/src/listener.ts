518d31e6727762816e929e9cd95ddcb591a55848
sammykirigha <dkirigha18@gmail.com>
2022-05-14 10:07:00 +0300

    randomly generated the client id

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,8 +1,8 @@
 import nats, { Message } from "node-nats-streaming";
-
+import {randomBytes} from 'crypto'
 console.clear();
 
-const stan = nats.connect("ticketing", "123", { url: "http://localhost:4222" });
+const stan = nats.connect("ticketing", randomBytes(4).toString('hex'), { url: "http://localhost:4222" });
 
 stan.on("connect", () => {
     console.log("am listening to you guys");

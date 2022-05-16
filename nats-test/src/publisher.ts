882af02bbcfc9723be81081313df0d6fc440b544
sammykirigha <dkirigha18@gmail.com>
2022-05-16 22:39:20 +0300

    refactored the code

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,18 +1,27 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 console.clear();
 const stan = nats.connect("ticketing", "abc", { url: "http://localhost:4222" }); 
 
 stan.on('connect', () => {
 	console.log('connected');
 
-	const data = JSON.stringify({
+
+	const publisher = new TicketCreatedPublisher(stan)
+	publisher.publish({
 		id: '123',
 		title: 'concert',
-		price: 20
+		price: 30
 	})
 
-	stan.publish('ticket:created', data, () => {
-		console.log('event published ');
-	})
+	// const data = JSON.stringify({
+	// 	id: '123',
+	// 	title: 'concert',
+	// 	price: 20
+	// })
+
+	// stan.publish('ticket:created', data, () => {
+	// 	console.log('event published ');
+	// })
 	
 })

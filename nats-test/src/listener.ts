3f9cca35ffecd399c1c264a8be8f53f881218ffd
sammykirigha <dkirigha18@gmail.com>
2022-05-16 10:24:48 +0300

    added an update

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -18,7 +18,8 @@ stan.on("connect", () => {
     const options = stan
         .subscriptionOptions()
         .setManualAckMode(true)
-        .setDeliverAllAvailable();
+        .setDeliverAllAvailable()
+        .setDurableName('accounting-service');
     const subscription = stan.subscribe(
         "ticket:created",
         "orders-service-queue-group"

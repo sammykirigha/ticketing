1cb54d77ba5bb62092e9e3b6a23c10fda9c277f6
sammykirigha <dkirigha18@gmail.com>
2022-05-17 23:29:34 +0300

    updates

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,18 +2,18 @@ import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/tickets";
 
-// it('it has a router handler listening to /api/tickets for post request', async () => {
+it('it has a router handler listening to /api/tickets for post request', async () => {
 
-// 	const response = await request(app).post('/api/tickets').send({})
-// 	expect(response.status).not.toEqual(404)
-// })
+	const response = await request(app).post('/api/tickets').send({})
+	expect(response.status).not.toEqual(404)
+})
 
 
-// it('it can be accessed if the user is signed in', async () => {
+it('it can be accessed if the user is signed in', async () => {
 
-// 	const response = await request(app).post('/api/tickets').set('Cookie', global.signin()).send({})
-// 	expect(response.status).toEqual(401)
-// })
+	const response = await request(app).post('/api/tickets').set('Cookie', global.signin()).send({})
+	expect(response.status).toEqual(401)
+})
 
 
 // it('return a status other than 401 if the user is signed in', async () => {

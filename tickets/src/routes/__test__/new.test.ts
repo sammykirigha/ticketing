97ebbf6cb99fe7b21af2ddf693a4c37a016f409e
sammykirigha <dkirigha18@gmail.com>
2022-05-17 23:30:12 +0300

    updates

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -16,21 +16,21 @@ it('it can be accessed if the user is signed in', async () => {
 })
 
 
-// it('return a status other than 401 if the user is signed in', async () => {
+it('return a status other than 401 if the user is signed in', async () => {
 
-// 	const response = await request(app).post('/api/tickets').send({})
-// 	expect(response.status).not.toEqual(401)
-// })
+	const response = await request(app).post('/api/tickets').send({})
+	expect(response.status).not.toEqual(401)
+})
 
 
-// it('it return an error if an invalid title is provided', async () => {
-// 	await  request(app).post('/api/tickets').set('Cookie', global.signin()).send({
-// 		title: '',
-// 		price: 10
-// 	})
-// 	.expect(400)
+it('it return an error if an invalid title is provided', async () => {
+	await  request(app).post('/api/tickets').set('Cookie', global.signin()).send({
+		title: '',
+		price: 10
+	})
+	.expect(400)
 
-// })
+})
 
 
 it('it return an error if an invalid price is provided', async () => {

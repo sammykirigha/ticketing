021d4fa71b36901710ed9461c6acee12b15f995c
sammykirigha <dkirigha18@gmail.com>
2022-05-17 23:30:44 +0300

    updates

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -11,21 +11,21 @@ it("returns a 404 if the ticket is not found", async () => {
         .expect(404);
 });
 
-// it("returns the ticket if the ticket is found", async () => {
-//     const title = "concert";
-//     const price = 20;
+it("returns the ticket if the ticket is found", async () => {
+    const title = "concert";
+    const price = 20;
 
-//     const response = await request(app)
-//         .post("/api/tickets")
-//         .set("Cookie", global.signin())
-//         .send({ title, price })
-//         .expect(201);
+    const response = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin())
+        .send({ title, price })
+        .expect(201);
 
-//     const ticketResponse = await request(app)
-//         .get(`/api/tickets/${response.body.id}`)
-//         .send()
-// 		.expect(200);
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+		.expect(200);
 	
-// 	expect(ticketResponse.body.title).toEqual(title);
-// 	expect(ticketResponse.body.price).toEqual(price);
-// });
+	expect(ticketResponse.body.title).toEqual(title);
+	expect(ticketResponse.body.price).toEqual(price);
+});

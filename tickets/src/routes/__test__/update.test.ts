a754405d8087b1c0b3682f834cc2f61ce6f03b43
sammykirigha <dkirigha18@gmail.com>
2022-05-17 23:31:46 +0300

    updates

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -14,16 +14,16 @@ it("return s a 404 is the provided doest not exist", async () => {
         .expect(404);
 });
 
-// it("return s a 401 if the user is not authenticated", async () => {
-//     const id = new mongoose.Types.ObjectId().toHexString();
-//     await request(app)
-//         .put(`/api/tickets/${id}`)
-//         .send({
-//             title: "hrbvkh",
-//             price: 20,
-//         })
-//         .expect(401);
-// });
+it("return s a 401 if the user is not authenticated", async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
+    await request(app)
+        .put(`/api/tickets/${id}`)
+        .send({
+            title: "hrbvkh",
+            price: 20,
+        })
+        .expect(401);
+});
 
 // it("returns a 401 if the user does not own the ticket", async () => {
 //     const response = await request(app)

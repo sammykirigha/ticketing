d5fcbd176f312af1978553e07e319b5c47b8e0db
sammykirigha <dkirigha18@gmail.com>
2022-05-24 17:53:14 +0300

    added routes and updated the app file

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,11 +2,10 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { createTicketRouter } from './routes/new'
-import {showTicketRouter} from './routes/show'
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
-
+import {deleteOrderRouter} from'./routes/delete'
+import { createOrderRouter } from './routes/new'
+import { showOrderRouter } from './routes/show'
+import {indexOrderRouter} from'./routes/index'
 import { errorHandler, NotFoundError, currentUser } from "@sdktickets/sammy";
 
 const app = express();
@@ -20,10 +19,10 @@ app.use(
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter)
-app.use(updateTicketRouter)
+app.use(createOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter)
+app.use(deleteOrderRouter)
 
 app.all("*", async () => {
     throw new NotFoundError();

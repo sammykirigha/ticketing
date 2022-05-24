328d6c6b8c5c0de0312d5f6cda83b1bd879d202e
sammykirigha <dkirigha18@gmail.com>
2022-05-24 17:17:57 +0300

    created order service

---
+++ orders/src/index.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+    // if (!process.env.JWT_KEY) {
+    //     throw new Error("JWT_KEY must be defined!!!!!");
+    // }
+
+    // if (!process.env.MONGO_URI) {
+    //     throw new Error("MONGO_URI must be definied");
+    // }
+
+    try {
+        await natsWrapper.connect('ticketing', 'sdnjdcfv', 'http://nats-srv:4222')
+        await mongoose.connect("mongodb://tickets-mongo-srv:27017/tickets");
+        natsWrapper.client.on("close", () => {
+            console.log("Nats connection is closed");
+            process.exit();
+        });
+
+        process.on("SIGINT", () => natsWrapper.client.close());
+        process.on("SIGTERM", () => natsWrapper.client.close());
+        console.log("Connected to MongoDb");
+    } catch (err) {
+        console.error(err);
+    }
+
+    app.listen(3000, () => {
+        console.log("App listening on port 3000!......");
+    });
+};
+
+start();

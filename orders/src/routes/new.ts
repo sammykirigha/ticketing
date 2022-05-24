c0681ec275ef3e20968e7d002aa9e9773d43bc75
sammykirigha <dkirigha18@gmail.com>
2022-05-24 18:26:34 +0300

    added create order functionality

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,18 @@
-import express, {Request, Response} from 'express';
+import mongoose from 'mongoose';
+import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@sdktickets/sammy'
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+	body('ticketId')
+		.not()
+		.isEmpty()
+		.custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+		.withMessage('Ticket id must be provided')
+], validateRequest,  async (req: Request, res: Response) => {
 	res.send({})
 })
 
-export {router as createOrderRouter}
+export { router as createOrderRouter }

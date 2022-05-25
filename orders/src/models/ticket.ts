a37b7148dc56c42114e2f9dd7a3d1cb2bfcb6aa0
sammykirigha <dkirigha18@gmail.com>
2022-05-25 17:46:52 +0300

    updates

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import { transform } from 'typescript';
+
+interface TicketAttr {
+	title: string;
+	prince: number
+}
+
+export interface TicketDoc extends mongoose.Document {
+	title: string;
+	price: string
+}
+
+export interface TicketModel extends mongoose.Model<TicketDoc>{
+	build(attrs:TicketAttr):TicketDoc
+}
+
+const ticketSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	price: {
+		type: Number,
+		required: true,
+		min: 0
+	}
+}, {
+	toJSON: {
+		transform(doc, ret){
+			ret.id = ret._id
+			delete ret._id
+		}
+	}
+})
+
+ticketSchema.statics.build = (attrs: TicketAttr) => {
+	return new Ticket(attrs)
+}
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema)
+
+export {Ticket}
+

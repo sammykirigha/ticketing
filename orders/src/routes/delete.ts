280b20c1662a2b4eb25fb40ff4fdedf68df4d039
sammykirigha <dkirigha18@gmail.com>
2022-05-30 20:38:49 +0300

    updates

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,13 +1,16 @@
 import { NotAuthorized, NotFoundError, OrderStatus } from '@sdktickets/sammy';
 import express, {Request, Response} from 'express';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
 import { Order } from '../models/order'
+import { Ticket } from '../models/ticket';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
 	const { orderId } = req.params
 	
-	const order = await Order.findById(orderId)
+	const order = await Order.findById(orderId).populate('ticket')
 
 	if (!order) {
 		throw new NotFoundError();
@@ -19,6 +22,14 @@ router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
 
 	order.status = OrderStatus.Cancelled;
 	await order.save()
+
+	new OrderCancelledPublisher(natsWrapper.client).publish({
+		id: order.id,
+		ticket: {
+			id: order.ticket.id,
+			price: order.ticket.price
+		}
+	})
 
 	res.status(204).send(order);
 })

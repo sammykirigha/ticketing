f5f64504c59e1ebe7541461ac06cbf4804449db3
sammykirigha <dkirigha18@gmail.com>
2022-06-13 15:31:00 +0300

    updates added

--- orders/src/events/listeners/ticket-created-lister.ts
+++ orders/src/events/listeners/ticket-created-lister.ts
@@ -10,5 +10,15 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
 
 	queueGroupName = queueGroupName;
 
-	onMessage(data: TicketCreatedEvent['data'], msg: Message){}
+	async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+		const { title, price } = data;
+
+		const ticket = Ticket.build({
+			title,
+			price
+		})
+		await ticket.save();
+
+		msg.ack();
+	}
 }
